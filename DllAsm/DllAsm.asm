54946668e660f01244fdfe0dfa32d4cb4d0bd67f
Mona Lisa <szbaczynski@gmail.com>
2022-04-23 19:26:54 +0200

    win

--- DllAsm/DllAsm.asm
+++ DllAsm/DllAsm.asm
@@ -1,3 +1,71 @@
+.data
+
+; data for ProcSrednia
+first dq 0
+sec dq 0
+dest dq 0
+
 .code
+
+Dodaj proc
+
+push rbx
+mov rbx, rcx
+mov rbx, [rbx]
+movups XMM0, [rbx]
+mov rbx, [rcx+8]
+movups XMM1, [rbx]
+addps xmm0, xmm1
+mov rbx, [rcx+16]
+movups [rbx], xmm0
+pop rbx
+ret
+Dodaj endp
+
+
+
+Odejmij proc
+push rbx
+mov rbx, rcx
+mov rbx, [rbx]
+movups XMM0, [rbx]
+mov rbx, [rcx+8]
+movups XMM1, [rbx]
+subps xmm0, xmm1
+mov rbx, [rcx+16]
+movups [rbx], xmm0
+pop rbx
+ret
+Odejmij endp
+
+
+Pomnoz proc
+push rbx
+mov rbx, rcx
+mov rbx, [rbx]
+movups XMM0, [rbx]
+mov rbx, [rcx+8]
+movups XMM1, [rbx]
+mulps xmm0, xmm1
+mov rbx, [rcx+16]
+movups [rbx], xmm0
+pop rbx
+ret
+Pomnoz endp
+
+
+Podziel proc
+push rbx
+mov rbx, rcx
+mov rbx, [rbx]
+movups XMM0, [rbx]
+mov rbx, [rcx+8]
+movups XMM1, [rbx]
+divps xmm0, xmm1
+mov rbx, [rcx+16]
+movups [rbx], xmm0
+pop rbx
+ret
+Podziel endp
 
 END
